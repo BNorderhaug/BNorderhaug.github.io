4c1feabb807548468e4a252de9b326d07067141c
BNorderhaug <bjorn.norderhaug@gmail.com>
2014-10-09 13:33:07 -0500

    3.2

--- todo.js
+++ todo.js
@@ -14,33 +14,44 @@
 		list.appendChild(newtaskli);
 		
 		checkbox.onclick = crossedOut;
+		localSave();
 	}
-	
-	function savedList() {
-	
-		list = JSON.parse(localStorage.getItem("toDoSave"));
-		var newtaskli = document.createElement("LI");
-		var checkbox = document.createElement("INPUT");
-		checkbox.type="checkbox";
-		newtaskli.appendChild(checkbox);
-		
-		var innerInput = document.getElementById("textbox1").value;
-		var outerContainer = document.createElement("SPAN");
-		
-		.shift
-	}
-	
-	window.onload = savedList;
 	
 	function crossedOut() {
 		parent=this.parentNode;
 		if (parent.className != "done") {
 			parent.className = "done";
+			localSave();
 		}
 		else {
 			parent.className = "";
+			localSave();
 		}
 	}
+	
+	function savedList() {
+	
+		list = JSON.parse(localStorage.getItem("toDoSave"));
+		while (list.length != 0) {
+			var newtaskli = document.createElement("LI");
+			var checkbox = document.createElement("INPUT");
+			checkbox.type="checkbox";
+			newtaskli.appendChild(checkbox);
+		
+			var innerInput = list[0];
+			var outerContainer = document.createElement("SPAN");
+			outerContainer.innerHTML = innerInput;
+			newtaskli.appendChild(outerContainer);
+		
+			var newList = document.getElementById("list");
+			newList.appendChild(newtaskli);
+		
+			list.shift();
+			checkbox.onclick = crossedOut;
+		}	
+	}
+	
+	window.onload = savedList;
 	
 	function localSave() {
 		res = [];
